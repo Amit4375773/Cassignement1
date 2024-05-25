1ef81515e8d961695a3efa28e36e42b69761e90c
Amit4375773 <amitkumarskd555@gmail.com>
2024-05-25 23:43:22 +0530

    Update Question9.c

--- Question9.c
+++ Question9.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int totalDays, years, weeks, days;
+    
+  printf("Number input: ");
+    scanf("%d", &totalDays);
+   
+  years = totalDays / 365;
+   
+  totalDays = totalDays % 365;
+    
+  weeks = totalDays / 7;
+   
+  days = totalDays % 7;
+   
+  printf("%d Years: %d Weeks: %d Days\n", years, weeks, days);
+    return 0;
+}
+//Amit Kumar
