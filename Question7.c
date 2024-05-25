8613f8dd7e6b78dc13dd1cf5785921e492e3f9f7
Amit4375773 <amitkumarskd555@gmail.com>
2024-05-25 23:39:59 +0530

    Update Question7.c

--- Question7.c
+++ Question7.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+// Write a C program that converts kilometres per hour to miles 
+per hour. (1 KM = 0.621371 M)
+int main() {
+    float kmPerHour, milesPerHour;
+    printf("Input kilometres per hour: ");
+    scanf("%f", &kmPerHour);
+    milesPerHour = kmPerHour * 0.621371;
+    printf("%.6f miles per hour\n", milesPerHour);
+    return 0;
+}
+//Amit Kumar 
