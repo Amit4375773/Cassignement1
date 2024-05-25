2ae01b0f8f1df51afbd88e0050a7aff696ae445d
Amit4375773 <amitkumarskd555@gmail.com>
2024-05-25 23:37:48 +0530

    Update Question5.c

--- Question5.c
+++ Question5.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+// Write a program to print “Happy Teachers Day” on the screen 
+(Remember to print double and single quotes).
+
+// Amit Kumar 
+int main() {
+    printf("\"Happy Teachers Day\"\n");
+    return 0;
+}
