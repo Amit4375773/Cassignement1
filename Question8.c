e58b09809083662160c46d990b3ab677a7d8edb5
Amit4375773 <amitkumarskd555@gmail.com>
2024-05-25 23:41:40 +0530

    Update Question8.c

--- Question8.c
+++ Question8.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main() {
+    int totalMinutes, hours, minutes;
+    printf("input total minutes: ");
+    scanf("%d", &totalMinutes);
+    hours = totalMinutes / 60;
+    minutes = totalMinutes % 60;
+    printf("%02d : %02d\n", hours, minutes);
+    return 0;
+}
+//Amit Kumar 
